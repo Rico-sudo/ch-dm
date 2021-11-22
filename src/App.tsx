a7f2c7d255055d03cf77e2943984191257322a9b
Rico-sudo <albericosss@hotmail.it>
2021-11-22 22:25:40 +1000

    first blood:

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+const ecg = [
+  {
+    name: '0 sec',
+    ecgVal: 180,
+  },
+  {
+    name: '5 sec',
+    ecgVal: 150,
+  },
+  {
+    name: '10 sec',
+    ecgVal: 120,
+  
+  },
+  {
+    name: '15 sec',
+    ecgVal: 70,
+
+  },
+  {
+    name: '20 sec',
+    ecgVal: 150,
+
+  },
+  {
+    name: '25 sec',
+    ecgVal: 60,
+ 
+  },
+  {
+    name: '30 sec',
+    ecgVal: 90,
+    // pv: 4300,
+    // amt: 2100,
+  },
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h2>Electrocardiogram</h2>
+    <ResponsiveContainer width="90%" aspect={3}>
+    <LineChart
+      width={500}
+      height={300}
+      data={ecg}
+      margin={{
+        top: 15,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid vertical={false}  stroke="#243240"/>
+      <XAxis dataKey="name" tick={{fill:"#fff"}}/>
+      <YAxis tick={{fill:"#fff"}} />
+      <Tooltip contentStyle={{backgroundColor:"#8884d8",color:"#fff"}} itemStyle={{color:"#fff"}} cursor={false} />
+      {/* legend will be usef when we will have multiple sensors */}
+      {/* <Legend />  */}
+      {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} /> */}
+      <Line type="monotone" dataKey="ecgVal" stroke="#8884d8" strokeWidth="2" dot={{fill:"2e4355" ,stroke:"#8884d8" ,strokeWidth:2 ,r:5}} activeDot={{ r: 8 }} />
+    </LineChart>
+  </ResponsiveContainer>
+  </>
   );
 }
 
